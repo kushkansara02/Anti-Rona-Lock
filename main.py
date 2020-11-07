449d32a7b4962516eccd90d0f2c395ce1cd5fe0c
David Xu <d83xu@uwaterloo.ca>
2020-11-07 18:14:27 -0500

    cleaned up main, another button

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 #I installed PIL (pillow) using this: pip3 install pillow
 #Not sure if pillow is needed anymore, tkinter might be enough
 import tkinter
-from .Social_Distancing_Detection import DistanceDetector
+# from Social_Distancing_Detection.image_handling.distance_detection import DistanceDetector
 
 def arduino_start():
     print("Arduino started.")
@@ -36,11 +36,11 @@ def mask_detection(image):
     return True
 
 def distance_detection(image):
-    detector = DistanceDetector(image)
-    detector.getCloseFaces()
-    allBreaches = detector.all_breaches
-    if len(allBreaches > 0):
-        return False #do not open door, they are not following rules
+    # detector = DistanceDetector(image)
+    # detector.getCloseFaces()
+    # allBreaches = detector.all_breaches
+    # if len(allBreaches > 0):
+    #     return False #do not open door, they are not following rules
     return True #open door
 
 #main is currently full of dummy functions that have yet to be actually implemented 
@@ -70,47 +70,71 @@ if __name__ == "__main__":
 
     # main()
 
-    image_list = []
-
     # image1 = Image.open("cat.png")
-    # image_list[0].show()
+    # image1.show()
+
+    image_list = []
 
     root = tkinter.Tk()
     root.title("Main")
 
-    canvas = tkinter.Canvas(root, width=1000, height=1000) 
+    CONST_CANVAS_WIDTH = 1000
+    CONST_CANVAS_HEIGHT = 1000
+    CONST_CANVAS_MAX_IMAGE_HEIGHT = 800
+    canvas = tkinter.Canvas(root, width=CONST_CANVAS_WIDTH, height=CONST_CANVAS_HEIGHT) 
     canvas.pack()     
     
-    img = tkinter.PhotoImage(file="cat.png") #png images done like this
-    img2 = ImageTk.PhotoImage(Image.open("cat2.jpg")) #jpg is not supported by tkinter, so gotta do it this way
-    image_list.append(img)
+    def new_size(size, desired_width=CONST_CANVAS_WIDTH):
+        width = size[0]
+        height = size[1]
+        new_width = desired_width
+        new_height = (int) ((desired_width/width)*height)
+        if new_height > CONST_CANVAS_MAX_IMAGE_HEIGHT:
+            new_height = CONST_CANVAS_MAX_IMAGE_HEIGHT
+            new_width = (int) ((CONST_CANVAS_MAX_IMAGE_HEIGHT/new_height)*new_width)
+        return (new_width, new_height)
+
+    def image(filename):
+        img_size = Image.open(filename).size
+        # print(img_size)
+        # print("new size: " + str(new_size(img_size)))
+        resized_img = Image.open(filename).resize(new_size(img_size))
+        img = ImageTk.PhotoImage(resized_img)
+        # alternative method: img = tkinter.PhotoImage(file="filename")
+        return img
+
+    img1 = image("cat1.png")
+    img2 = image("cat2.jpg")
+    img3 = image("cat3.jpg")
+    image_list.append(img1)
     image_list.append(img2)    
-
-    # canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[0])   
-    # canvas.pack() 
-    # root.update()
-
-    # sleep(2)
-
-    # canvas.delete("all")
-    # canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[1]) 
-    # canvas.pack()
-    # root.update()
+    image_list.append(img3)
 
     i = 0
-    def change_image():
+    canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])   
+
+    def next_image():
         canvas.delete("all")
         global i
-        canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])
-        root.update() 
         i += 1
         if i >= len(image_list):
             i = 0
+        canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])
+        root.update() 
 
-    button = tkinter.Button(root, text="Next Image", command=change_image)
-    button.place(x=480,y=800)
+    def prev_image():
+        canvas.delete("all")
+        global i
+        i -= 1
+        if i < 0:
+            i = len(image_list)-1
+        canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])
+        root.update() 
+        
+
+    button = tkinter.Button(root, text="Next Image", command=next_image)
+    button.place(x=500,y=900)
+    button = tkinter.Button(root, text="Previous Image", command=prev_image)
+    button.place(x=400,y=900)
 
     root.mainloop()  
-
-    # image2 = Image.open("cat2.jpg")
-    # image2.show()
