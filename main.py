ed3bc52a43723cb9c0ff615df30becc6a0f418dd
David Xu <d83xu@uwaterloo.ca>
2020-11-10 20:40:14 -0500

    figured out how to run main along with tkinter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#whole bunch of other imports here
 from time import sleep
 from PIL import Image, ImageTk
-#I installed PIL (pillow) using this: pip3 install pillow
-#Not sure if pillow is needed anymore, tkinter might be enough
+#I installed PIL (pillow) using "pip3 install pillow"
 import tkinter
-# from Social_Distancing_Detection.image_handling.distance_detection import DistanceDetector
+from Social_Distancing_Detection.image_handling.distance_detection import DistanceDetector
+import os
+
+#below are dummy functions
 
 def arduino_start():
     print("Arduino started.")
@@ -16,7 +17,7 @@ def arduino_get_image():
 
 def arduino_range_sensor():
     #will tell us if there is an object in front of the door
-    print("There is someone at the door")
+    print("There is someone at the door.")
     return True
 
 def arduino_unlock_door():
@@ -32,10 +33,11 @@ def arduino_stop():
     return
 
 def mask_detection(image):
-    print("Masks are being worn.")
-    return True
+    print("Masks are not being worn.")
+    return False
 
 def distance_detection(image):
+    print("Distance is being maintained.")
     # detector = DistanceDetector(image)
     # detector.getCloseFaces()
     # allBreaches = detector.all_breaches
@@ -43,35 +45,31 @@ def distance_detection(image):
     #     return False #do not open door, they are not following rules
     return True #open door
 
-#main is currently full of dummy functions that have yet to be actually implemented 
-def main():
-    arduino_start()
-    locked = True
-    wait_time = 1
-    while locked:
-        if arduino_range_sensor():
-            image = arduino_get_image()
-            masks = mask_detection(image)
-            distancing = distance_detection(image)
-            if (masks and distancing):
-                arduino_unlock_door()
-                locked = False
-            else:
-                print("Violation detected. Waiting " + str(wait_time) + " seconds before getting new image.")
-                sleep(wait_time)
+# def main():
+#     arduino_start()
+#     locked = True
+#     wait_time = 1
+#     while locked:
+#         if arduino_range_sensor():
+#             image = arduino_get_image()
+#             masks = mask_detection(image)
+#             distancing = distance_detection(image)
+#             if (masks and distancing):
+#                 arduino_unlock_door()
+#                 locked = False
+#             else:
+#                 print("Violation detected. Waiting " + str(wait_time) + " seconds before getting new image.")
+#                 sleep(wait_time)
             
-    sleep(10)
-    arduino_lock_door()
-    arduino_stop()
-    print("Program Finished.")
+#     sleep(5)
+#     arduino_lock_door()
+#     arduino_stop()
+#     print("Program Finished.")
 
 
 if __name__ == "__main__":
-
+    
     # main()
-
-    # image1 = Image.open("cat.png")
-    # image1.show()
 
     image_list = []
 
@@ -94,18 +92,22 @@ if __name__ == "__main__":
             new_width = (int) ((CONST_CANVAS_MAX_IMAGE_HEIGHT/new_height)*new_width)
         return (new_width, new_height)
 
-    def image(filename):
-        img_size = Image.open(filename).size
+    def image(filename, location):
+        abs_path = location + filename
+        img_size = Image.open(abs_path).size
         # print(img_size)
         # print("new size: " + str(new_size(img_size)))
-        resized_img = Image.open(filename).resize(new_size(img_size))
+        resized_img = Image.open(abs_path).resize(new_size(img_size))
         img = ImageTk.PhotoImage(resized_img)
         # alternative method: img = tkinter.PhotoImage(file="filename")
         return img
 
-    img1 = image("cat1.png")
-    img2 = image("cat2.jpg")
-    img3 = image("cat3.jpg")
+    images_abs_path = os.path.dirname(os.path.abspath(__file__)) + "\\images\\"
+
+    img1 = image("cat1.png", images_abs_path)
+    img2 = image("cat2.jpg", images_abs_path)
+    img3 = image("cat3.jpg", images_abs_path)
+
     image_list.append(img1)
     image_list.append(img2)    
     image_list.append(img3)
@@ -120,7 +122,6 @@ if __name__ == "__main__":
         if i >= len(image_list):
             i = 0
         canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])
-        root.update() 
 
     def prev_image():
         canvas.delete("all")
@@ -129,7 +130,6 @@ if __name__ == "__main__":
         if i < 0:
             i = len(image_list)-1
         canvas.create_image(0,0, anchor=tkinter.constants.NW, image=image_list[i])
-        root.update() 
         
 
     button = tkinter.Button(root, text="Next Image", command=next_image)
@@ -137,4 +137,26 @@ if __name__ == "__main__":
     button = tkinter.Button(root, text="Previous Image", command=prev_image)
     button.place(x=400,y=900)
 
+    #this code replaces main()
+    arduino_start()
+    wait_time = 3
+    def while_loop():
+        if arduino_range_sensor():
+            image = arduino_get_image()
+            # image_list.append(image)
+            next_image()
+            masks = mask_detection(image)
+            distancing = distance_detection(image)
+            if (masks and distancing):
+                arduino_unlock_door()
+                sleep(5)
+                arduino_lock_door()
+                arduino_stop()
+                print("Program Finished.")
+            else:
+                print("Violation detected. Waiting " + str(wait_time) + " seconds before trying again.")
+                root.after(wait_time*1000, while_loop)
+        
+    root.after(wait_time*1000, while_loop)
+    
     root.mainloop()  
